96a9d8ac8cc869b7bcd60b0eab2f2e49f759aabc
withpouriya <pouriya.raj@gmail.com>
2023-01-22 21:10:01 +0330

    feat(all_sprites): add offset

--- frogger/all_sprites.py
+++ frogger/all_sprites.py
@@ -3,7 +3,10 @@ import pygame
 class AllSprites(pygame.sprite.Group):
 	def __init__(self):
 		super().__init__()
+		self.offset = pygame.math.Vector2()
 
 	def customize_draw(self, surf):
+
 		for sprite in self.sprites():
-			surf.blit(sprite.image, sprite.rect)
+			offset_pos = sprite.rect.topleft + self.offset
+			surf.blit(sprite.image, offset_pos)
